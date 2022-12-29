e1bce4ee527152209f04f36aada267fb906138fd
Awi <102220981+Awi22@users.noreply.github.com>
2022-12-29 16:05:43 +0800

    Add Animated_splash_screen

---
+++ lib/splash.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const MyHomePage(
+                  title: 'Retail Kumala',
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // ignore: avoid_unnecessary_containers
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              color: Colors.red,
+            ),
+            Container(
+              child: const Text(
+                'AWI',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
